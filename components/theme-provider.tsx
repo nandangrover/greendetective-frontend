d0fbab0fe6727e911bb8d84fb24075f134cf2bec
Nandan Grover <nandangrover.5@gmail.com>
2025-02-08 23:46:26 +0000

    theme fix

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -9,7 +9,7 @@ export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
     <NextThemesProvider
       attribute="class"
       defaultTheme="dark"
-      enableSystem
+      enableSystem={false}
       themes={["light", "dark"]}
       {...props}
     >
